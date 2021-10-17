ee537b470f839e61aa10f710576e7f194ac97e50
Erik Agsjö <erik.agsjo@gmail.com>
2021-10-17 17:08:14 +0200

    All the warnings

--- ci/build.sh
+++ ci/build.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-CFLAGS="-Wall -pedantic"
+CFLAGS="-Wall -Wextra -pedantic -Wno-missing-field-initializers"
 
 mkdir -p build
 
-gcc -std=c99 -g args.c example/example.c -o build/cexample
-g++ -std=c++11 -x c++ args.c example/example.c -o build/c++example
-
-g++ -std=c++11 -x c++ -g args.c tests/test.cpp -o build/runtests
+# Should build clean in both C99 and C++11
+gcc -std=c99 $CFLAGS -g args.c example/example.c -o build/cexample
+g++ -std=c++11 $CFLAGS -x c++ args.c example/example.c -o build/c++example
+g++ -std=c++11 $CFLAGS -x c++ -g args.c tests/test.cpp -o build/runtests
